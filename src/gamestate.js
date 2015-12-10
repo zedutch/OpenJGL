daa63e10a6f460bddcd7edc7473b3f01ec0687e8
Robin Arys <contact@zedutchgandalf.be>
2015-12-10 01:57:07 +0100

    Add a GameState super class.

---
+++ src/gamestate.js
@@ -0,0 +1,26 @@
+function GameState() {
+    "use strict";
+}
+
+//The following functions do nothing since they're intended to be overwritten.
+
+GameState.prototype.update = function update(deltaTime) {
+    "use strict";
+};
+
+GameState.prototype.render = function render(screen) {
+    "use strict";
+};
+
+GameState.prototype.willDisappear = function willDisappear() {
+    "use strict";
+};
+
+GameState.prototype.willAppear = function willAppear() {
+    "use strict";
+};
+
+GameState.prototype.toString = function () {
+    "use strict";
+    return "GameState";
+};
