cce7e57a38a5c3520e4bd9195eb439fadca32f8c
Robin Arys <contact@zedutchgandalf.be>
2015-12-12 00:43:08 +0100

    Move addition tests to a separate test

--- tests/math.js
+++ tests/math.js
@@ -15,12 +15,22 @@ QUnit.test( "Vector2 Creation", function(assert) {
     assert.ok( v1.y == y1, "Vector creation for vector 1: y ok" );
     assert.ok( v2.x == x2, "Vector creation for vector 2: x ok" );
     assert.ok( v2.y == y2, "Vector creation for vector 2: y ok" );
+});
+
+QUnit.test( "Vector2 Addition", function (assert) {
+    var v1 = getRandomVector();
+    var x1 = v1.x;
+    var y1 = v1.y;
     
-    // Test vector addition
+    var v2 = getRandomVector();
+    var x2 = v2.x;
+    var y2 = v2.y;
+    
     v1.add(v2);
     
     assert.ok( v1.x == x1 + x2, "Vector addition for vector 1: x ok" );
     assert.ok( v1.y == y1 + y2, "Vector addition for vector 1: y ok" );
     assert.ok( v2.x == x2, "Vector addition for vector 2: x ok" );
     assert.ok( v2.y == y2, "Vector addition for vector 2: y ok" );
+});
 });
