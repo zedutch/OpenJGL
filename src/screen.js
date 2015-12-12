f3d4ed475fdd63823979ef80b9c3322e7f33a2df
Robin Arys <contact@zedutchgandalf.be>
2015-12-12 11:53:21 +0100

    Formatting + general cleanup

--- src/screen.js
+++ src/screen.js
@@ -1,15 +1,16 @@
 function Screen(canvas, resolution) {
-    
+    "use strict";
+
     if (!(canvas instanceof HTMLCanvasElement)) {
         throw new InvalidArgumentError("canvas", "Invalid canvas object passed: " + canvas);
     }
-    
+
     this.size = new Vector2(canvas.width, canvas.height);
     this.font = "HelveticaNeue Liberation Roboto Sans";
     this.fontColor = "#FFF";
     this.fontSize = 20;
-    
-    if ( !(resolution instanceof Vector2) ) {
+
+    if (!(resolution instanceof Vector2)) {
         this.resolution = this.size;
     } else {
         this.resolution = resolution;
@@ -18,7 +19,7 @@ function Screen(canvas, resolution) {
     var context = canvas.getContext("2d");
 
     Log.info("Screen initialized. Canvas Size: " + this.size + " Resolution: " + this.resolution, this);
-    
+
     this.render = function (image, position, size) {
         context.drawImage(image, position.x, position.y, size.x, size.y);
     };
@@ -27,17 +28,16 @@ function Screen(canvas, resolution) {
         context.clearRect(0, 0, this.size.x, this.size.y);
     };
 
-    this.renderText = function(text, position, size, colour, font) {
+    this.renderText = function (text, position, size, colour, font) {
         if (typeof font === 'undefined') {
             font = this.font;
         } else if (typeof font !== 'string') {
             throw new InvalidArgumentError("font", "Fonts have to be strings containing both the font size and one or more font families. Received: " + font);
         }
-        
+
         if (typeof size === 'undefined') {
             size = this.fontSize;
         } else if (typeof size !== 'number') {
-            console.log(typeof size);
             throw new InvalidArgumentError("size", "size must be an integer. Received: " + size);
         }
 
@@ -47,9 +47,9 @@ function Screen(canvas, resolution) {
             throw new InvalidArgumentError("colour", "Colours have to be strings. Received: " + colour);
         }
 
-        if ( typeof position === 'undefined') {
+        if (typeof position === 'undefined') {
             position = new Vector2();
-        } else if ( !(position instanceof Vector2) ) {
+        } else if (!(position instanceof Vector2)) {
             throw new InvalidArgumentError("position", "Position should be a Vector2. Received: " + position);
         }
 
@@ -60,5 +60,6 @@ function Screen(canvas, resolution) {
 }
 
 Screen.prototype.toString = function () {
-    return "Screen"
+    "use strict";
+    return "Screen";
 };
